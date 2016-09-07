3a8f30c57c7443ecd10c2c163a95c43120cb2587
aedobbyn <adobbs11@gmail.com>
2016-09-07 16:52:02 -0500

    format table

--- scrape.R
+++ scrape.R
@@ -56,6 +56,80 @@ length(wiki_table) # a list of 2
 #+
 gdp <- wiki_table[[2]]
 
+#' Rename columns with Euro symbols
+gdp <- gdp %>% 
+  rename(
+    GDP = `GDP €`,
+    GDP_percap = `GDP per person €`
+  )
+gdp
+
+#' #' Take out last row with totals
+gdp <- gdp[1:(nrow(gdp) - 1), ]
+
+
+#' Check out table structure
+str(gdp)
+
+#' Make tibble
+gdp <- as_tibble(gdp)
+
+
+#' Take out Euro symbols in rows and "bn" for billion in GDP column
+gdp$GDP <- str_replace_all(gdp$GDP, "€", "")
+gdp$GDP <- str_replace_all(gdp$GDP, "bn", "")
+gdp$GDP_percap <- str_replace_all(gdp$GDP_percap, "€", "")
+
+
+gdp2 <- gdp
+#' Replace "m" (in Population) with scientific notation
+gdp2$Population <- gdp$Population %>% 
+  gsub(" m", "e+06", .) 
+  # as.numeric(.)
+#   format(scientific=FALSE)
+gdp2$Population
+
+# gdp2$population <- if("e" %in% gdp2$population) {as.numeric(gdp2$population)}
+
+gdp2$Population[1] <- as.numeric(gdp2$Population[1])
+format(gdp2$Population[1], scientific = TRUE)
+
+
+gdp2
+
+#' Set variable data types
+
+gdp3 <- gdp2
+gdp3$Area <- factor(gdp$Area)
+
+gdp3$GDP_percap <- parse_number(gdp$GDP_percap)
+gdp3$GDP <- parse_number(gdp$GDP)
+gdp3$Population <- parse_number(gdp2$Population)
+gdp3$City <- factor(gdp$City)
+gdp3$Country <- factor(gdp$Country)
+
+str(gdp3)
+
+
+
+
+# repl.m <- function(pop) {
+#   for (r in pop) {
+#     if (contains("m")) {
+#       str_replace(., "m", "000000")
+#     } else {
+#       next
+#     }
+#   }
+# }
+# gdp2$population <- repl.m(gdp$Population)
+
+
+
+
+
+
+
 #' ***
   
 #' Scrape all text (excluding citations) from the Wikipedia page
